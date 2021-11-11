69c5f582c68c7890e6fa698e4012004feb44300a
Elmartino4 <49419740+Elmartino4@users.noreply.github.com>
2021-11-11 14:46:09 +1100

    implemented max sizing for the map

--- core/src/github/elmartino4/guncorp/map/MapScreen.java
+++ core/src/github/elmartino4/guncorp/map/MapScreen.java
@@ -10,24 +10,27 @@ import github.elmartino4.guncorp.ScreenAdapter;
 import github.elmartino4.guncorp.menus.EscapeMenu;
 import github.elmartino4.guncorp.menus.MenuData;
 
-import java.util.Arrays;
 import java.util.function.Consumer;
 
 public class MapScreen extends ScreenAdapter {
-    private static final float ACCELERATION = 8;
+    private static final float ACCELERATION = 0.2F;
+    private static final float MIN_VELOCITY = 2.5F;
     private static final int EDGE = 60;
     private static final int GRID = 48;
+    private static final float MAP_SIZE = 40;
     private final Menu[] MENUS;
 
     private int currentMenu = -1;
-    private final int[] pos = new int[2];
+    private final float[] pos = new float[2];
     private final float[] velocity = new float[2];
-    public final MapData mapData = new MapData(69420);
+    public final MapData mapData = new MapData(69);
     public Consumer<MenuData> menuDataConsumer;
 
     public MapScreen(Consumer<MenuData> consumer) {
         menuDataConsumer = consumer;
-        MENUS = new Menu[] { new EscapeMenu(consumer) };
+        MENUS = new Menu[]{
+                new EscapeMenu(consumer)
+        };
     }
 
     @Override
@@ -42,20 +45,24 @@ public class MapScreen extends ScreenAdapter {
         if (currentMenu == -1) {
             if (Gdx.input.getX() < EDGE || Gdx.input.isKeyPressed(Input.Keys.LEFT)
                     || Gdx.input.isKeyPressed(Input.Keys.A)) {
-                velocity[0] -= Gdx.graphics.getDeltaTime() * ACCELERATION;
+                if (velocity[0] == 0) velocity[0] = -MIN_VELOCITY;
+                velocity[0] -= ACCELERATION;
             } else if (Gdx.input.getX() > Gdx.graphics.getWidth() - EDGE || Gdx.input.isKeyPressed(Input.Keys.RIGHT)
                     || Gdx.input.isKeyPressed(Input.Keys.D)) {
-                velocity[0] += Gdx.graphics.getDeltaTime() * ACCELERATION;
+                if (velocity[0] == 0) velocity[0] = MIN_VELOCITY;
+                velocity[0] += ACCELERATION;
             } else {
                 velocity[0] = 0;
             }
 
-            if (Gdx.input.getY() < EDGE || Gdx.input.isKeyPressed(Input.Keys.DOWN)
+            if (Gdx.input.getY() > Gdx.graphics.getHeight() - EDGE || Gdx.input.isKeyPressed(Input.Keys.DOWN)
                     || Gdx.input.isKeyPressed(Input.Keys.S)) {
-                velocity[1] -= Gdx.graphics.getDeltaTime() * ACCELERATION;
-            } else if (Gdx.input.getY() > Gdx.graphics.getHeight() - EDGE || Gdx.input.isKeyPressed(Input.Keys.UP)
+                if (velocity[1] == 0) velocity[1] = -MIN_VELOCITY;
+                velocity[1] -= ACCELERATION;
+            } else if (Gdx.input.getY() < EDGE || Gdx.input.isKeyPressed(Input.Keys.UP)
                     || Gdx.input.isKeyPressed(Input.Keys.W)) {
-                velocity[1] += Gdx.graphics.getDeltaTime() * ACCELERATION;
+                if (velocity[1] == 0) velocity[1] = MIN_VELOCITY;
+                velocity[1] += ACCELERATION;
             } else {
                 velocity[1] = 0;
             }
@@ -64,15 +71,22 @@ public class MapScreen extends ScreenAdapter {
         if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE))
             currentMenu = (currentMenu != -1) ? -1 : 0;
 
-        pos[0] += velocity[0];
-        pos[1] += velocity[1];
+        pos[0] += velocity[0] * Gdx.graphics.getDeltaTime();
+
+        pos[1] += velocity[1] * Gdx.graphics.getDeltaTime();
+
+        pos[0] = Math.max(Math.min(pos[0], MAP_SIZE), -MAP_SIZE);
+
+        pos[1] = Math.max(Math.min(pos[1], MAP_SIZE), -MAP_SIZE);
 
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
 
-        for (int x = 0; x <= Gdx.graphics.getWidth() / GRID + 1; x++) {
-            for (int y = 0; y <= Gdx.graphics.getHeight() / GRID + 1; y++) {
-                Color color = mapData.getColor(x + pos[0], y + pos[1]);
-                shapeRenderer.rect(x * GRID, y * GRID, GRID, GRID, color, color, color, color);
+        for (int x = -1; x <= Gdx.graphics.getWidth() / GRID + 1; x++) {
+            for (int y = -1; y <= Gdx.graphics.getHeight() / GRID + 1; y++) {
+                Color color = mapData.getColor(x + (int)pos[0], y + (int)pos[1]);
+                shapeRenderer.rect((x - pos[0] % 1) * GRID,
+                        (y - pos[1] % 1) * GRID,
+                        GRID, GRID, color, color, color, color);
             }
         }
 
@@ -85,7 +99,7 @@ public class MapScreen extends ScreenAdapter {
 
     @Override
     public String getDebugText() {
-        String out = Gdx.input.isTouched() + "\nPos: " + Arrays.toString(pos);
+        String out = String.format("Pos: %.1f, %.1f", pos[0], pos[1]);
         if (currentMenu != -1) {
             out += "\n" + MENUS[currentMenu].getDebugText();
         }
