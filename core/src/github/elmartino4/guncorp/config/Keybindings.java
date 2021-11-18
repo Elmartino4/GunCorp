55634d51b6be3c1fb7fd8ea1d41071892b4609b1
stampyzfanz <34364128+stampyzfanz@users.noreply.github.com>
2021-11-18 16:39:16 +1100

    Add mouse buttons and fix previous code

--- core/src/github/elmartino4/guncorp/config/Keybindings.java
+++ core/src/github/elmartino4/guncorp/config/Keybindings.java
@@ -8,18 +8,19 @@ public class Keybindings {
     public static Preferences kb;
 
     public static void generate() {
-        kb = Gdx.app.getPreferences("GunCorpkb.xml");
+        kb = Gdx.app.getPreferences("GunCorpKeybindings.xml");
         if (kb.get().size() == 0) {
-            kb.putString("Up", "w, up");
-            kb.putString("Left", "a, left");
-            kb.putString("Right", "d, right");
-            kb.putString("Down", "s, down");
+            kb.putString("Up", "W, Up");
+            kb.putString("Left", "A, Left");
+            kb.putString("Right", "D, Right");
+            kb.putString("Down", "S, Down");
             kb.putString("Toggle debug", "F3");
             kb.putString("Exit menu", "Escape");
             kb.putString("Go to left menu", "mousebutton_RIGHT");
             kb.putString("Go to right menu", "mousebutton_LEFT");
             kb.putBoolean("Autoscrolling mouse", true);
         }
+        kb.flush();
     }
 
     public static boolean isKeyJustPressed(String name) {
@@ -31,19 +32,30 @@ public class Keybindings {
     }
 
     private static boolean isKeyPressedGeneric(String name, boolean isRepeating) {
-        for (String key : kb.getString(name).strip().split(",")) {
+        if (!kb.contains(name)) {
+            throw new RuntimeException("Programming error: Action type " + name + " does not exist");
+        }
+
+        for (String key : kb.getString(name).replaceAll("\\s", "").split(",")) {
             if (key.startsWith("mousebutton_")) {
                 int keycode;
                 try {
-                    keycode = ReflectionUtil.getFieldValue(Input.Buttons.class, key.replace("mousebutton_", ""));
+                    keycode = ReflectionUtil.<Integer>getStaticFieldValue(Input.Buttons.class,
+                            key.replace("mousebutton_", ""));
                 } catch (NoSuchFieldException | IllegalAccessException e) {
-                    e.printStackTrace();
+                    throw new RuntimeException(e);
                 }
                 if (isRepeating ? Gdx.input.isButtonPressed(keycode) : Gdx.input.isKeyJustPressed(keycode)) {
                     return true;
                 }
             } else {
                 int keycode = Input.Keys.valueOf(key);
+
+                if (keycode == -1) {
+                    throw new RuntimeException("Keybinding error: Key \"" + key
+                            + "\" does not exist as field in Gdx.Input.Keys https://libgdx.badlogicgames.com/ci/nightlies/docs/api/com/badlogic/gdx/Input.Keys.html");
+                }
+
                 if (isRepeating ? Gdx.input.isKeyPressed(keycode) : Gdx.input.isKeyJustPressed(keycode)) {
                     return true;
                 }
